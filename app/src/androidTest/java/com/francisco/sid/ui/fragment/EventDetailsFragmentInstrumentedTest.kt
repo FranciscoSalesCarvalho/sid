364f72760c94e3f9293747a0f9eb7c317b51ba24
Francisco Sales <francisco.sales@rarolabs.com.br>
2021-03-14 15:20:40 -0400

    testing share function

--- app/src/androidTest/java/com/francisco/sid/ui/fragment/EventDetailsFragmentInstrumentedTest.kt
+++ app/src/androidTest/java/com/francisco/sid/ui/fragment/EventDetailsFragmentInstrumentedTest.kt
@@ -6,6 +6,11 @@ import androidx.navigation.NavDeepLinkBuilder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -15,6 +20,10 @@ import androidx.test.rule.ActivityTestRule
 import com.francisco.sid.R
 import com.francisco.sid.data.model.Event
 import com.francisco.sid.ui.activity.MainActivity
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Matcher
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -53,12 +62,34 @@ class EventDetailsFragmentInstrumentedTest {
             .check(matches(isDisplayed()))
     }
 
+    @Test
+    fun shouldShare() {
+        Intents.init()
+
+        onView(withId(R.id.share))
+            .perform(click())
+
+        val expectedIntent = allOf(
+            hasAction(Intent.ACTION_SEND),
+            hasType("text/plain")
+        )
+        intended(chooser(expectedIntent))
+
+        Intents.release()
+    }
+
+    private fun chooser(matcher: Matcher<Intent>): Matcher<Intent> {
+        return allOf(
+            hasAction(Intent.ACTION_CHOOSER),
+            hasExtra(`is`(Intent.EXTRA_INTENT), matcher))
+    }
+
     companion object {
-        fun default(): Event = Event(
+        private fun default(): Event = Event(
             "1",
             "Event Default",
             "This is a event default used for tests",
-            "",
+            "http",
             1L,
             1.0,
             1.0,
